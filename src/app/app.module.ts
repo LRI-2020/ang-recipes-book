78a588da5b6d0e857419ffb5145fce9983537fdb
LaylaR <layla.richard.2111@gmail.com>
2023-11-27 13:28:20 +0100

    creating all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import { RecipesComponent } from './recipes/recipes.component';
+import {RecipesListComponent} from "./recipes/recipes-list/recipes-list.component";
+import {RecipeItemComponent} from "./recipes/recipes-list/recipe-item/recipe-item.component";
+import {RecipeDetailsComponent} from "./recipes/recipe-details/recipe-details.component";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {ShoppingListEditComponent} from "./shopping-list/shopping-list-edit/shopping-list-edit.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
