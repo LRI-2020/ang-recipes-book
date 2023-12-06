2a9ae350103a35f190d9e37e88189da8fa44f460
LaylaR <layla.richard.2111@gmail.com>
2023-12-06 15:09:30 +0100

    list ingredients of recipe in edition

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,9 +1,9 @@
-import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {RecipesService} from "../../../services/recipes.service";
 import {ActivatedRoute} from "@angular/router";
-import {isEmpty} from "../../shared/helpers";
-import {Ingredient} from "../../shared/ingredient";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {v4 as uuidv4} from "uuid";
 
 @Component({
   selector: 'app-recipe-edit',
@@ -11,10 +11,10 @@ import {Ingredient} from "../../shared/ingredient";
   styleUrl: './recipe-edit.component.scss'
 })
 export class RecipeEditComponent {
-  editMode=false;
-  originalRecipe: Recipe;
-  @ViewChild('new_name') newName:ElementRef;
-  @ViewChild('new_description') newDescription:ElementRef;
+  editMode = false;
+  originalRecipe: Recipe = null;
+
+  editRecipeForm: FormGroup;
 
   constructor(private recipeService: RecipesService, private activeRoute: ActivatedRoute) {
   }
@@ -22,25 +22,25 @@ export class RecipeEditComponent {
   ngOnInit(): void {
     this.activeRoute.params.subscribe((params) => {
       this.editMode = params['id'] != null;
-      if(this.editMode){
+      if (this.editMode) {
         let id = +params['id'];
         this.originalRecipe = this.recipeService.getRecipeById(id);
       }
-
-    })
+    });
+    this.generateForm();
 
   }
 
   onSave() {
-    if (!isEmpty(this.newName.nativeElement.value)
-      && !isEmpty(this.newDescription.nativeElement.value)
-      && Number(this.activeRoute.snapshot.params['id']) === this.originalRecipe.id) {
-      this.recipeService.updateRecipe(this.originalRecipe.id, this.newName.nativeElement.value, this.newDescription.nativeElement.value);
-      alert('changes are saved');
-    } else {
-      alert('there is an error in your data');
-
-    }
+    // if (!isEmpty(this.newName.nativeElement.value)
+    //   && !isEmpty(this.newDescription.nativeElement.value)
+    //   && Number(this.activeRoute.snapshot.params['id']) === this.originalRecipe.id) {
+    //   this.recipeService.updateRecipe(this.originalRecipe.id, this.newName.nativeElement.value, this.newDescription.nativeElement.value);
+    //   alert('changes are saved');
+    // } else {
+    //   alert('there is an error in your data');
+    //
+    // }
   }
 
   onCancel() {
@@ -49,5 +49,44 @@ export class RecipeEditComponent {
 
   onBack() {
 
+  }
+
+  onAddIngredient(){
+    const formGroup = new FormGroup({
+        'ingredientName': new FormControl(null),
+        'ingredientAmount': new FormControl(null)
+      });
+    (<FormGroup>this.editRecipeForm.get('ingredients')).addControl(uuidv4(),formGroup);
+  }
+
+  private generateForm() {
+
+    let ingredients:FormGroup<{}> = this.generateIngredients();
+    this.editRecipeForm = new FormGroup({
+      'recipeId': new FormControl(this.editMode ? this.originalRecipe.id : ''),
+      'recipeName': new FormControl(this.editMode ? this.originalRecipe.name : ''),
+      'recipeDescription': new FormControl(this.editMode ? this.originalRecipe.description : ''),
+      'ingredients': ingredients
+    })
+  }
+
+  private generateIngredients() {
+    let ingredients = new FormGroup({});
+
+    if (this.editMode) {
+      this.originalRecipe.ingredients.forEach(ing => {
+
+        let ingredientForm = new FormGroup({
+          'ingredientName': new FormControl(ing.name),
+          'ingredientAmount': new FormControl(ing.amount)
+        })
+        ingredients.addControl(ing.id, ingredientForm);
+      });
+    }
+    return ingredients;
+  }
+
+  getIngredientForms() {
+    return ((<FormGroup>this.editRecipeForm.get('ingredients')).controls);
   }
 }
